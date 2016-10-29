dd5da6e56578b932a7e61e976a8cac55a93ce793
Tung-Thai <anhtungthai181292@gmail.com>
2016-10-29 18:29:44 +0700

    Update train_hoa.py

--- train_hoa.py
+++ train_hoa.py
@@ -1,8 +1,10 @@
+#http://machinelearningmastery.com/multi-class-classification-tutorial-keras-deep-learning-library/
 import numpy
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.constraints import maxnorm
 from keras.optimizers import SGD
@@ -17,8 +19,6 @@ numpy.random.seed(seed)
 # load pima indians dataset
 dataframe = pandas.read_csv("/tmp/codegit/AWS2/hoa-header.csv", header=None)
 dataset = dataframe.values
-
-
 # split into input (X) and output (Y) variables
 X = dataset[:,0:4]
 Y = dataset[:,4]
@@ -27,17 +27,6 @@ encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 dummy_y = np_utils.to_categorical(encoded_Y)
-# define 10-fold cross validation test harness
-# define baseline model
-def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
-	model.add(Dense(3, init='normal', activation='sigmoid'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
-estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
-kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
-results = cross_val_score(estimator, X, dummy_y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+#dummy_y = dummy_y.shape[150,3]
+print dummy_y
+
