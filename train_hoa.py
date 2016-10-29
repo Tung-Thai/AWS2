a32a6f7f2ed363266b05e74da9a25b8fae5b6ca2
Tung-Thai <anhtungthai181292@gmail.com>
2016-10-29 17:59:13 +0700

    Update train_hoa.py

--- train_hoa.py
+++ train_hoa.py
@@ -1,3 +1,7 @@
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.constraints import maxnorm
@@ -16,29 +20,24 @@ dataset = dataframe.values
 
 
 # split into input (X) and output (Y) variables
-X = dataset[:,0:5]
-Y = dataset[:,5]
+X = dataset[:,0:4]
+Y = dataset[:,4]
 #conver str class colum to interger
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
+dummy_y = np_utils.to_categorical(encoded_Y)
 # define 10-fold cross validation test harness
-kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-cvscores = []
-for train, test in kfold.split(X, encoded_Y):
-  # create model
+# define baseline model
+def baseline_model():
+	# create model
 	model = Sequential()
-	
-	model.add(Dense(12, input_dim=4, init='normal', activation='relu'))
-	model.add(Dense(5, init='normal', activation='relu'))
-	model.add(Dense(1, init='normal', activation='sigmoid'))
+	model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
+	model.add(Dense(3, init='normal', activation='sigmoid'))
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	# Fit the model
-	model.fit(X[train], encoded_Y[train], nb_epoch=20, batch_size=6, verbose=0)
-	# evaluate the model
-	scores = model.evaluate(X[test], encoded_Y[test], verbose=0)
-	print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-	cvscores.append(scores[1] * 100)
-
-print("%.2f%% (+/- %.2f%%)" % (numpy.mean(cvscores), numpy.std(cvscores)))
+	return model
+estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
