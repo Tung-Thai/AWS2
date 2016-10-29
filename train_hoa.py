ae299fc36a2fc86c8bab7dcbd93442acbfdcce83
Tung-Thai <anhtungthai181292@gmail.com>
2016-10-29 18:34:16 +0700

    Update train_hoa.py

--- train_hoa.py
+++ train_hoa.py
@@ -17,7 +17,7 @@ from sklearn.pipeline import Pipeline
 seed = 7
 numpy.random.seed(seed)
 # load pima indians dataset
-dataframe = pandas.read_csv("/tmp/codegit/AWS2/hoa-header.csv", header=None)
+dataframe = pandas.read_csv("/tmp/codegit/AWS2/hoa.csv", header=None)
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
 X = dataset[:,0:4]
@@ -27,6 +27,19 @@ encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 dummy_y = np_utils.to_categorical(encoded_Y)
-#dummy_y = dummy_y.shape[150,3]
-print dummy_y
+#dummy_y = dummy_y.shape[1]
+# define 10-fold cross validation test harness
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(4, input_dim=4, init='normal', activation='relu'))
+	model.add(Dense(3, init='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+estimator = KerasClassifier(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=0)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
